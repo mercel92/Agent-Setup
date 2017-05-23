12152afe18ec0f57366b177c2eed9e0e04d8f7ef
MUSTAFA ERÇEL <mercelnet@gmail.com>
2017-05-23 17:38:42 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -31,7 +31,7 @@ git pull
 EOFMARKER7
 
 chmod u+x /usr/src/tagent/tagent-update.sh
-echo 'nohup python /usr/src/tagent/agent.py && nohup python /usr/src/tagent/update.py >/dev/null 2>&1' >> /usr/src/tagent/tagent-update.sh
+echo 'nohup python /usr/src/tagent/agent.py && nohup python /usr/src/tagent/update.py >/dev/null 2>&1 &' >> /usr/src/tagent/tagent-update.sh
 
 mv /usr/src/server_ip.txt /usr/src/tagent/
-nohup python /usr/src/tagent/agent.py && nohup python /usr/src/tagent/update.py >/dev/null 2>&1
+nohup python /usr/src/tagent/agent.py && nohup python /usr/src/tagent/update.py >/dev/null 2>&1 &
